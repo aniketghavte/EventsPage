72260416fc40b92685ec2aa8ff0292d9ae91c0d1
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2022-12-04 19:09:58 +0530

    card js css

---
+++ src/components/cards/Card.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import fk from './fk.jpg';
+
+import './Card.css';
+export default function card() {
+  return (
+    <>
+      <section class="service" id="service">
+        <div class="heading">
+            <h2>Let's Look at the events</h2>
+        </div>
+
+        <div class="service-content">
+            <div class="row">
+                <div class=" s s-one">
+                <img src={fk} className="event_logo" alt="logo" />
+                </div>
+                <h3>Event 1</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                </p>
+                <h4>24 Dec</h4>
+            </div>
+            <div class="row">
+                <div class=" s s-one">
+                <img src={fk} className="event_logo" alt="logo" />
+                </div>
+                <h3>Event 2</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <h4>24 Dec</h4>
+            </div>
+            <div class="row">
+                <div class=" s s-one">
+                <img src={fk} className="event_logo" alt="logo" />       
+                </div>
+                <h3>Event 3</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <h4>24 Dec</h4>
+            </div>
+            <div class="row">
+                <div class=" s s-one">
+                <img src={fk} className="event_logo" alt="logo" />         
+                </div>
+                <h3>Event 4</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <h4>24 Dec</h4>
+            </div>
+            <div class="row">
+                <div class=" s s-one">
+                <img src={fk} className="event_logo" alt="logo" />         
+                </div>
+                <h3>Event 5</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <h4>24 Dec</h4>
+            </div>
+            <div class="row">
+                <div class=" s s-one">
+                <img src={fk} className="event_logo" alt="logo" />         
+                </div>
+                <h3>Event 6</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <h4>24 Dec</h4>
+            </div>
+            <div class="row">
+                <div class=" s s-one">
+                <img src={fk} className="event_logo" alt="logo" />         
+                </div>
+                <h3>Event 7</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <h4>24 Dec</h4>
+            </div>
+            <div class="row">
+                <div class=" s s-one">
+                <img src={fk} className="event_logo" alt="logo" />          
+                </div>
+                <h3>Event 8</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <h4>24 Dec</h4>
+            </div>
+        </div>
+        <div></div>
+    </section>
+    </>
+  )
+}
